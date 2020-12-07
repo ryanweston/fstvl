fe88c3ea79d59313f7d1af39590758b74d908bd6
Ryan Weston <ryanlweston@gmail.com>
2020-12-07 21:39:40 +0000

    added background circles

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,34 @@
 <template>
   <div id="app">
-   <h1>FIND FESTIVALS NEAREST TO YOU</h1>
-    <input 
-          type="text" 
-          placeholder="Search..."
-          v-model="query"
-          @keypress="convertPostcode"
-        />
-    <input 
-          type="range"
-          min="0"
-          max="100"
-          v-model="range"
-          />
-    <p>Range: {{ range }}</p>
-    <h3>Results</h3>
-    <div v-for="item in events" :key="item.eventname">{{ item.eventname }}</div>
+    <div class="container">
+      <h1>FIND FESTIVALS NEAREST TO YOU</h1>
+
+      <div class="search">
+        <input 
+              type="text" 
+              placeholder="Search..."
+              v-model="query"
+              @keypress="convertPostcode"
+              class="text-search"
+            />
+        <input 
+              type="range"
+              min="0"
+              max="100"
+              v-model="range"
+              class="radius-search"
+              />
+        <p>Range: {{ range }}</p>
+        </div>
+        <div class="circle-1"></div>
+        <div class="circle-2"></div>
+        <h3>Results</h3>
+        <div v-for="item in events" :key="item.eventname">{{ item.eventname }}</div>
+    </div>
+
+    <footer>
+
+    </footer>
   </div>
 </template>
 
@@ -68,10 +81,67 @@ export default {
 }
 body {
   font-family: Viga;
+  margin:0px;
+  overflow:hidden;
+}
+.container {
+  padding:5%;
+  box-sizing: border-box;
 }
 h1 {
   font-size:8.5em;
   line-height:92%;  
-  /* margin-bottom:0px; */
+  margin-bottom:0px;
+}
+.search {
+  width:65%;
+  height:auto;
+  position: relative;
+  box-sizing: border-box;
+}
+.text-search {
+  width:100%;
+  border:4px solid #000000;
+  outline:none;
+  padding:20px;
+  background:transparent;
+}
+.radius-search {
+  position:absolute;
+  top:0px;
+  right:0px;
+}
+.circle-1, .circle-2{
+  position:absolute;
+  width: 718px;
+  height: 718px;
+  border-radius:50%;
+}
+.circle-1 {
+  top:0px;
+  right:0px;
+  background: rgba(235, 255, 0, 0.58);
+  z-index:-1;
+  transform: translate(-15%, -15%);
+  -webkit-transform: translate(-15%, -15%);
+  -moz-transform: translate(-15%, -15%);
+  -o-transform: translate(-15%, -15%); 
+}
+.circle-2 {
+  bottom:0px;
+  right:0px;
+  z-index:-2;
+  background: rgba(251, 156, 14, 0.58);
+  transform: translate(25%, 25%);
+  -webkit-transform: translate(25%, 25%);
+  -moz-transform: translate(25%, 25%);
+  -o-transform: translate(25%, 25%); 
+}
+footer {
+  position:absolute;
+  bottom:0px;
+  width:100%;
+  border-top:4px solid #000000;
+  height:100px;
 }
 </style>
